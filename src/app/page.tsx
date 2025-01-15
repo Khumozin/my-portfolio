53eb0a3aa1f34593da2c2a04b337ca7ef0b2650f
Khumo Mogorosi <khumomogorosi@gmail.com>
2025-01-15 21:01:41 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,111 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const yearsOfExp = new Date().getFullYear() - new Date(2018, 1, 18).getFullYear();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    return (
+      <main className="main">
+      <div className="content">
+    <div className="left-side">
+
+<Image
+      src="/khumo.jpeg"
+      width={150}
+      height={150}
+      alt="Picture of the author"
+      className='profile-photo'
+      priority
+    />
+
+      <h1>Khumo Mogorosi</h1>
+      <p>
+        An enthusiastic technical professional with a complete understanding of the entire software development
+        lifecycle.
+      </p>
+
+      <p>
+        As a software developer with over <b>{ yearsOfExp } years of experience</b>, I have developed a strong
+        expertise in Angular, Nx
+        Monorepo, TypeScript, Javascript, Cypress (E2E Automation),
+        Jasmine (Unit Testing), and DevSecOps. I am passionate about building scalable and maintainable software
+        solutions that
+        solve real-world problems.
+      </p>
+
+      <h2>Work experiences:</h2>
+
+      <p>
+        Currently, I&apos;m a <b>Software Developer</b> at <Link className="pill"
+           href="https://www.toyota.co.za/"
+           target="_blank"
+           rel="noopener">Toyota SA Motors</Link>, and I&apos;m responsible for building, optimizing and
+        improving enterprise scale Angular applications.
+      </p>
+
+      <p>Things I&apos;ve worked on:</p>
+
+      <p>
+        - Build standalone Angular libraries<br/>
+        - Application runtime performance optimizations<br/>
+        - Code architecture improvements<br/>
+        - Code modernization with latest Angular features<br/>
+        - Migrating legacy apps to Angular app<br/>
+        - Unit and E2E testing Angular apps<br/>
+        - Release Management (Semantic Versioning)
+      </p>
+
+      <p>
+        Using Angular with a variety of libraries and other frameworks like Electron, NgRx, Angular Material,
+        NgBootstrap,
+        Tailwind CSS, .Net,
+        Node.js, and more, targeting all platforms including iOS, Android, Desktop apps, etc,. is something I&apos;m very
+        familiar with.
+      </p>
+
+      <div className="built">
+        <h6 className='mr-4'>Built with </h6>
+
+        <Image src="/next.svg"
+      width={150}
+      height={150}
+      alt="Picture of the author"
+      priority></Image>
+      </div>
     </div>
+    <div className="divider"
+         role="separator"
+         aria-label="Divider"></div>
+    <div className="right-side">
+      <div className="pill-group">
+        {
+            [
+                { title: 'Github Profile', link: 'https://github.com/khumozin' },
+                { title: 'Stack Overflow', link: 'https://stackoverflow.com/users/9674861/khumo-mogorosi' },
+                { title: 'LinkedIn', link: 'https://www.linkedin.com/in/khumo-mogorosi-8ab477145/' },
+                { title: 'Medium Blog', link: 'https://medium.com/@khumomogorosi' },
+                ].map((item) => {
+                    return  <Link key={item.title} className="pill flex"
+                    href="{item.link}"
+                    target="_blank"
+                    rel="noopener">
+          <span>{ item.title }</span>
+          <svg xmlns="http://www.w3.org/2000/svg"
+               height="14"
+               viewBox="0 -960 960 960"
+               width="14"
+               fill="currentColor">
+            <path
+                  d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
+          </svg>
+        </Link>
+                })
+        }
+      </div>
+    </div>
+  </div>
+</main>
   );
 }
---
+++ src/app/sitemap.ts
@@ -0,0 +1,6 @@
+export default async function sitemao() {
+    return [{
+        url: 'https://khumo-mog.vercel.app',
+        lastModified: new Date()
+    }]
+}
